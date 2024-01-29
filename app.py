478083a9a539159bb21815b0d8b9881a070329dd
Jacob Conklin <jacobyconklin@gmail.com>
2024-01-29 15:08:08 -0500

    Pathways for hosting, joining, and viewing members of a session with db integration completed

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 import os
-import pyodbc as odbc
+import pyodbc 
+import random
 from dotenv import load_dotenv
 load_dotenv()
+from datetime import datetime
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for, jsonify, json)
 from flask_cors import CORS
+from environmentSecrets import AZURE_SQL_CONNECTION_STRING
 
 app = Flask(__name__)
 CORS(app)
 
 # Temporary global variables before database is connected
 serverStorageCount = 0
-users = []
+users = [] # May still be usefulf for watching and printing data in compared to data in db, but not currently used.
 
 @app.route('/')
 def index():
@@ -36,15 +39,13 @@ def hello():
        return redirect(url_for('index'))
    
 
-# My methods:
+# STSELab Demo Methods:
 @app.route('/addcount')
 def addCount():
     global serverStorageCount
     serverStorageCount += 1
     return jsonify({"count": serverStorageCount})
 
-
-# STSELab Demo Methods:
 @app.route('/submitform', methods=['POST'])
 def submitform():
     try:
@@ -61,7 +62,7 @@ def submitform():
         return jsonify({"success": True, "user": newUser})
     except Exception as e:
         print(e)
-        return jsonify({"error": e})
+        return jsonify({"error": str(e)})
     
 @app.route('/allusers')
 def allusers():
@@ -76,19 +77,141 @@ def resetusers():
 
 @app.route('/testdb')
 def testdb():
-    connection_string = os.getenv('AZURE_SQL_CONNECTIONSTRING')
-    conn = odbc.connect(connection_string)
-    cursor = conn.cursor()
-    # cursor.execute(f"INSERT INTO Persons (FirstName, LastName) VALUES (?, ?)", "lim", "Kob")
-    # conn.commit()
+    # ODBC works for local windows maching with ODBC Driver 18. Change to Driver={FreeTDS} for deployed Linux
+    # driver_string = 'Driver={ODBC Driver 18 for SQL Server};' if os.name == 'nt' else 'Driver={FreeTDS};'
+    # connection_string = driver_string + os.getenv('AZURE_SQL_CONNECTIONSTRING')
+    try:
+        # Create connection to SQL Database
+        conn = pyodbc.connect(AZURE_SQL_CONNECTION_STRING, timeout=120)
+        cursor = conn.cursor()
+        totalString = "people are: "
+        # Execute SQL query and build result
+        cursor.execute("SELECT * FROM TestUsers")
+        for row in cursor.fetchall():
+            totalString += row.FirstName + " " + row.color + ", "
+        return jsonify({"people": totalString})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
-    totalString = "people are: "
-    cursor.execute("SELECT * FROM Persons")
-    for row in cursor.fetchall():
-        print(row.FirstName, row.LastName)
-        totalString += row.FirstName + " " + row.LastName + ", "
-    return jsonify({"people": totalString})
 
+
+# All real methods to be used by application:
+
+# TODO set up routing and controllers so this file isn't out of hand
+    
+# TODO Needed endpoints:
+    # Principally: Status session
+
+    # begin game (for hosts)
+    # join game (for players)
+    # remove player (for hosts)
+    # leave session (for players)
+    # end tournament (for hosts) OR maybe don't allow them to end early as it could make data collection weirder?
+    # begin round (for hosts)
+    # end round (for hosts) - may need to forcibly close rounds even if players aren't done
+        # TODO work on session dates, make them actual UTC timestamps, and switch to using startDate to be when the tournament has started
+        # So that no players can join a session partly through. Then use the endDate so that the front-end polling the session can know
+        # when the session is over. Also use round to tell what round the session is on (or could just make round 0 and round 4 represent
+        # start and end time and not need those fields, but may be nice to have when the sessions actually happened). 
+    # record hole result (for all)
+        # TODO decide if FE or BE hits Plumber, if BE then endpoint for recording hole will take in all necessary params
+        # Such as round #, solvers, etc, and will call R then record hole, otherwise just save results from FE
+    # get scores for round for session (for all)
+    # get scores for all 3 rounds for session (for all)
+    # get aggregate scores for all sessions (for all)
+    # get aggregate scores for for a player (for all) ? Maybe not needed and not sure exactly what it would look like
+
+# Begins a new game by a host, creating a session. Will need all of the information collected for each player, without
+# a join code. Must return if successful or not, and if successful, the join code for the session.
+@app.route('/host', methods=['POST'])
+def host():
+    try:
+        # TODO decide on exact data wanted for each player
+        # First check that required data is in request
+        data = request.json
+        firstName = data.get('firstName')
+        color = data.get('color')
+
+        # Now generate a join code and create a new session in the database
+        joinCode = random.randint(100000, 999999)
+        # TODO check join code doesn't already exist in database
+
+        # Create connection to Azure SQL Database
+        conn = pyodbc.connect(AZURE_SQL_CONNECTION_STRING, timeout=120)
+        cursor = conn.cursor()
+
+        # Insert new session into database
+        cursor.execute(f"SET IDENTITY_INSERT Session ON INSERT INTO Session (joinCode, startDate, endDate) VALUES (?, ?, ?)",
+            (str(joinCode), datetime.today().strftime('%Y-%m-%d'), 'None'))
+        conn.commit()
+
+        # Now create new user and insert them into the database
+        cursor.execute(f"INSERT INTO TestUsers (firstName, sessionId, color) VALUES (?, ?, ?)", (firstName, str(joinCode), color))  
+        conn.commit()
+
+        # On success return success and join code
+        return jsonify({"success": True, "joinCode": joinCode})
+    except Exception as e:
+        print(e)
+        return jsonify({"error": str(e)})
+    
+# Joins a game creating a player for a given session IF that session exists
+@app.route('/join', methods=['POST'])
+def join():
+    try:
+        # TODO decide on exact data wanted for each player
+        # First check that required data is in request, must have valid join code
+        data = request.json
+        firstName = data.get('firstName')
+        color = data.get('color')
+        joinCode = data.get('joinCode')
+
+        # Create connection to Azure SQL Database
+        conn = pyodbc.connect(AZURE_SQL_CONNECTION_STRING, timeout=120)
+        cursor = conn.cursor()
+
+        # Check that session exists
+        cursor.execute(f"SELECT * FROM Session WHERE joinCode = ?", (str(joinCode)))
+        session = cursor.fetchone()
+        if session is None:
+            return jsonify({"error": "Session not found"})
+
+        # Now create new user and insert them into the database
+        cursor.execute(f"INSERT INTO TestUsers (firstName, sessionId, color) VALUES (?, ?, ?)", (firstName, str(joinCode), color))  
+        conn.commit()
+
+        return jsonify({"success": True})
+    except Exception as e:
+        print(e)
+        return jsonify({"error": str(e)})
+
+# Retreives all players in a given Session
+@app.route('/session/players', methods=['POST'])
+def sessionPlayers():
+    try:
+        # First check that required data is in request, must have valid sessionId
+        data = request.json
+        sessionId = data.get('sessionId')
+
+        # Create connection to Azure SQL Database
+        conn = pyodbc.connect(AZURE_SQL_CONNECTION_STRING, timeout=120)
+        cursor = conn.cursor()
+
+        # Check that players exist with matching sessionId
+        cursor.execute(f"SELECT * FROM TestUsers WHERE sessionId = ?", (str(sessionId)))
+        players = cursor.fetchall()
+        if not players:
+            return jsonify({"error": "Players not found"})
+        
+        playerList = []
+        for player in players:
+            print(player.firstName)
+            playerList.append({"id": player.id, "firstName": player.firstName, "color": player.color})
+
+        return jsonify({"success": True, "players": playerList})
+    except Exception as e:
+        print(e)
+        return jsonify({"error": str(e)}) 
 
 
 if __name__ == '__main__':
