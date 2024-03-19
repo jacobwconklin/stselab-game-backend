f08f18b5330ed7997ab85eeb57b655122c1b2ab1
Jacob Conklin <jacobyconklin@gmail.com>
2024-03-19 09:47:20 -0400

    Working websocket that preserves all existing routes, untested deployed

--- app.py
+++ app.py
@@ -10,18 +10,32 @@ from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for, jsonify, json)
 from flask_cors import CORS
 from environmentSecrets import AZURE_SQL_CONNECTION_STRING
+from flask_socketio import SocketIO, emit, join_room, leave_room, send
 
 app = Flask(__name__)
-CORS(app)
+app.config['SECRET_KEY'] = 'secret!'
+CORS(app,resources={r"/*":{"origins":"*"}})
+socketio = SocketIO(app,cors_allowed_origins="*")
+
+# app = Flask(__name__)
+# CORS(app)
+# app.config['SECRET_KEY'] = 'stselab'
+# socketio = SocketIO(app)
+# socketio.init_app(app, cors_allowed_origins="*")
 
 # Temporary global variables before database is connected
-serverStorageCount = 0
-users = [] # May still be usefulf for watching and printing data in compared to data in db, but not currently used.
+# serverStorageCount = 0
+# users = [] # May still be usefulf for watching and printing data in compared to data in db, but not currently used.
 
 @app.route('/')
 def index():
    print('Request for index page received')
    return render_template('index.html')
+
+@app.route('/ws')
+def websocket():
+   print('Request for websocket page received')
+   return render_template('websocket.html')
 
 @app.route('/favicon.ico')
 def favicon():
@@ -99,14 +113,118 @@ app.add_url_rule('/dap_arch', 'dap_arch', dap_arch, methods=['POST'])
 app.add_url_rule('/ds_arch', 'ds_arch', ds_arch, methods=['POST'])
 
 
-# TODO may switch to websocket connection for backend with https://flask-socketio.readthedocs.io/en/latest/
-# Websocket endpoints:
-# Using websockets allows server to send messages, so clients do not have to continually poll for updates.
-# The updates that the server may need to send are:
-    # A player has joined a session,
-    # A player has left a session,
-    # And the session status changing (starting, round changing, ending)
+# # TODO may switch to websocket connection for backend with https://flask-socketio.readthedocs.io/en/latest/
+# # Websocket endpoints:
+# # Using websockets allows server to send messages, so clients do not have to continually poll for updates.
+# # The updates that the server may need to send are:
+#     # A player has joined a session,
+#     # A player has left a session,
+#     # And the session status changing (starting, round changing, ending)
 
+
+# # Websocket event catchers:
+
+# # Will be using names instead of namespaces I believe
+
+# @socketio.on('join')
+# def on_join(data):
+#     username = data['username']
+#     room = data['room']
+#     join_room(room)
+#     send(username + ' has entered the room.', to=room)
+
+# @socketio.on('leave')
+# def on_leave(data):
+#     username = data['username']
+#     room = data['room']
+#     leave_room(room)
+#     send(username + ' has left the room.', to=room)
+
+# # When a message is sent with the broadcast option enabled, all clients connected to the namespace receive it, including the sender. When namespaces are not used, the clients connected to the global namespace receive the message. 
+
+# # The socketio.send() and socketio.emit() methods can be used to broadcast to all connected clients BASED on events that happen server side. The context-free socketio.send() and socketio.emit() functions also accept a to argument to broadcast to all clients in a room. All clients are assigned a room when they connect, named with the session ID of the connection, which can be obtained from request.sid
+# # socketio.emit("emitting to the rooom", to=request.sid)
+
+# # handles custom event called "my_custom_event" with 3 arguments (num of args can be anything)
+# @socketio.event
+# def my_custom_event(arg1, arg2, arg3):
+#     print('received args: ' + arg1 + arg2 + arg3)
+#     # Use emit for custom events
+#     # by default will use namespace of incoming message, but 
+#     # namespace can also be specified (Like group)
+#     # emit('my response', json)
+#     # To send an event with multiple arguments, send a tuple:
+#     # emit('my response', ('foo', 'bar', json), namespace='/chat')
+#     # Broadcast to entire namespace via
+#     emit('my response', json, broadcast=True)
+
+# # on_event method allows separate function definition
+# def my_function_handler():
+#     socketio.emit("Hello room!", to=request.sid)
+
+# socketio.on_event('say hi to room', my_function_handler)
+
+# # Catches unnamed events with string messages
+# @socketio.on('message')
+# def handle_message(data):
+#     print('received message: ' + data)
+#     send(data)
+
+# # catches unnamed events with json messages
+# @socketio.on('json')
+# def handle_json(json):
+#     print('received json: ' + str(json))
+#     send(json, json=True)
+
+# # Connection events: (probably unnecessary but I want them for logging)
+# @socketio.on('connect')
+# def test_connect():
+#     print('Client connected')
+#     emit('my response', {'data': 'Connected'})
+
+# @socketio.on('disconnect')
+# def test_disconnect():
+#     print('Client disconnecting')
+#     print('Client disconnected')
+    
+
+# if __name__ == '__main__':
+#     socketio.run(app)
+
+
+@app.route("/http-call")
+def http_call():
+    """return JSON with string data as the value"""
+    data = {'data':'This text was fetched using an HTTP call to server on render'}
+    return jsonify(data)
+
+@socketio.on("connect")
+def connected():
+    """event listener when client connects to the server"""
+    print(request.sid)
+    print("client has connected")
+    emit("connect",{"data":f"id: {request.sid} is connected"})
+
+@socketio.on("custom event 1")
+def custom_event_1(data):
+    """event listener when client emits a custom event"""
+    print("custom event 1: ",data)
+    if (data == "ping"):
+        emit("custom event 1","pong",broadcast=True)
+    else: 
+        emit("custom event 1","wrong",broadcast=True)
+
+@socketio.on('data')
+def handle_message(data):
+    """event listener when client types a message"""
+    print("data from the front end: ",str(data))
+    emit("data",{'data':data,'id':request.sid},broadcast=True)
+
+@socketio.on("disconnect")
+def disconnected():
+    """event listener when client disconnects to the server"""
+    print("user disconnected")
+    emit("disconnect",f"user {request.sid} disconnected",broadcast=True)
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True,port=5000)
